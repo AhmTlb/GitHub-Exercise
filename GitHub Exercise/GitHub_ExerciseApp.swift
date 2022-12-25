042f641436fd2500c410cb25dc5dfba3fd9ecb3f
AhmTlb <ahmtalab@gmail.com>
2022-12-25 15:39:55 +0300

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by MacBook Air on 01/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
